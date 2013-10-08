1062cbcde40ed22b55a95b5cda288a368a7aec2b
Mohit Thakral <mohitthakral@DellMacBookPro.local>
2013-10-08 16:57:37 -0500

    Initial Upload

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('shopApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
